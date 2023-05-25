a3638d2ed3c59329248a847df09115f84825df2b
Max Edell <maxed@adobe.com>
2023-05-25 14:12:01 -0700

    fix: tweak releaserc

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -16,17 +16,17 @@ module.exports = {
       },
     ],
     [
-      '@semantic-release/git',
-      {
-        assets: ['package.json', 'CHANGELOG.md', 'prisma/prisma-cloud/docs/sitemaps/**/*'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
-    [
       '@semantic-release/exec',
       {
         prepareCmd: "echo 'generating sitemaps' && npm run gen:sitemaps",
         publishCmd: "echo 'deploy worker' && npm run deploy",
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md', 'prisma/prisma-cloud/docs/sitemaps/**/*'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
   ],
