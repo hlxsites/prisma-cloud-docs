a82dfec0360eaee94370ae3a16be38e922d8867d
icaraps <icaraps@hotmail.com>
2023-06-20 15:47:54 +0200

    add book adoc reference check

--- bin/preview-changes.js
+++ bin/preview-changes.js
@@ -1,30 +1,86 @@
+import fs from 'node:fs';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import yaml from 'js-yaml';
+
 /**
  * Generates preview URLs as GitHub comments for adoc changes
  * or displays a default preview URL if only book changes
  *
  * @param github
  * @param context
+ * @param glob
  * @param changes
  * @returns {Promise<void>}
  */
-export default async function previewChanges({ github, context, changes }) {
+export default async function previewChanges({
+  github, context, glob, changes,
+}) {
   const host = 'https://main--prisma-cloud-docs-website--hlxsites.hlx.page';
   const fallbackPath = '/prisma/prisma-cloud/en';
   const branch = context.payload.pull_request.head.ref;
 
-  // We only care about adoc changes for preview urls
-  const adocChanges = changes.filter((change) => change.endsWith('.adoc'));
+  const bookGlobber = await glob.create('docs/**/book.yml');
+  const books = await bookGlobber.glob();
+
+  const adocGlobber = await glob.create('docs/**/*.adoc');
+  const adocs = await adocGlobber.glob();
+
+  // Find all references adoc files in books
+  const adocReferences = new Set();
+  const normalizePath = (path) => path.toLowerCase().replaceAll('_', '-');
+  const buildAdocReferencePath = (doc, currentPath) => {
+    if (doc.file) {
+      currentPath.push(normalizePath(doc.file));
+      return currentPath.join('/');
+    }
+
+    if (doc.dir) {
+      currentPath.push(normalizePath(doc.dir));
+    }
+
+    if (doc.topics) {
+      doc.topics.forEach((topic) => {
+        const path = buildAdocReferencePath(topic, [...currentPath]);
+        if (path) {
+          adocReferences.add(path);
+        }
+      });
+    }
+
+    return null;
+  };
+
+  books.forEach((book) => {
+    const docs = yaml.loadAll(fs.readFileSync(book).toString()).filter((doc) => doc?.kind === 'chapter');
+    docs.forEach((doc) => {
+      buildAdocReferencePath(doc, book.split('/').slice(0, -1));
+    });
+  });
+
+  // Compare adoc files with adoc referenced in books
+  const missingReferences = [...adocReferences]
+    .filter((adocReference) => !adocs.includes(adocReference));
+
   // Remove "docs" and ".adoc" from path
-  const cleanPath = (file) => file.slice(4, -5);
+  const cleanChangePath = (file) => file.slice(4, -5);
+
+  let body = changes.length ? `Preview URL(s):\n\n${changes.map((change) => `- ${host}/prisma/prisma-cloud${cleanChangePath(change)}?branch=${branch}`).join('\n')}`
+    : `Default Preview URL: ${host}${fallbackPath}?branch=${branch}`;
+
+  if (missingReferences.length) {
+    // Remove home/runner/work/repo/dir
+    const cleanRefPath = (ref) => ref.split('/').slice(6).join('/');
+
+    const buildGHLink = (path) => `https://github.com/${context.payload.pull_request.base.repo.full_name}/tree/${branch}/${cleanRefPath(path)}`;
+
+    body += `\n\nWarning: ${missingReferences.length} missing adoc reference(s) found:\n\n`;
+    body += `${missingReferences.map((missingReference) => `- <a href="${buildGHLink(missingReference)}" target="_blank">${cleanRefPath(missingReference)}</a>`).join('\n')}`;
+  }
 
   await github.rest.issues.createComment({
     issue_number: context.issue.number,
     owner: context.repo.owner,
     repo: context.repo.repo,
-    body: adocChanges.length ? `
-Preview URL(s):
-    
-${adocChanges.map((change) => `- ${host}/prisma/prisma-cloud${cleanPath(change)}?branch=${branch}`).join('\n')}`
-      : `Default Preview URL: ${host}${fallbackPath}?branch=${branch}`,
+    body,
   });
 }
