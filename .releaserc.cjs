3973bab7c3d79c6af95251f16d56c5eb1b72ceaa
Stephan Ringel <icaraps@hotmail.com>
2023-06-28 18:37:07 +0200

    chore: add lock file to semantic-release (#54)

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     [
-      '@semantic-release/changelog', 
+      '@semantic-release/changelog',
       {
         changelogFile: 'CHANGELOG.md',
       }
@@ -24,7 +24,7 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        assets: ['package.json', 'CHANGELOG.md'],
+        assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
