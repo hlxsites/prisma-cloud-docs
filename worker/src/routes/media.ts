8f79fff107eca293ba1af0d065223880c21f5d5e
Max Edell <maxed@adobe.com>
2023-10-09 13:15:38 -0700

    fix: workaround for incorrect mime types for some gifs

--- worker/src/routes/media.ts
+++ worker/src/routes/media.ts
@@ -8,11 +8,19 @@ const Media: Route = async (req, ctx) => {
   const upstream = `${await resolveURL(ctx.url.pathname, ctx)}`;
   log.debug('[Media] upstream: ', upstream);
 
-  const resp = await fetch(upstream);
+  let resp = await fetch(upstream);
   if (!resp.ok) {
     if (resp.status === 404) {
       return undefined;
     }
+  }
+  resp = new Response(resp.body, resp);
+  const contentType = resp.headers.get('content-type');
+  // Workaround for some gifs showing as octet-stream mime type from Github;
+  // force them to be image/gif if they have the appropriate extension.
+  // This could be extended for other mime types if needed, but for now gifs is enough.
+  if (ctx.url.pathname.endsWith('.gif') && contentType === 'application/octet-stream') {
+    resp.headers.set('content-type', 'image/gif');
   }
   return resp;
 };
