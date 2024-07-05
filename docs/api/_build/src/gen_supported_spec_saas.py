e0bf13463f0dc8408c82f74b5b9c20b161ea2a77
smitapaloalto <156162707+smitapaloalto@users.noreply.github.com>
2024-07-05 12:56:16 +0530

    oneal-update-7-API-changes

--- docs/api/_build/src/gen_supported_spec_saas.py
+++ docs/api/_build/src/gen_supported_spec_saas.py
@@ -75,7 +75,6 @@ def gen_spec(spec_file, config_file):
 
   # Generate a spec that contains supported endpoints only.
   filter_spec(config.spec)
-  filter_env_details(config.spec)
   remove_x_public(config.spec)
   return config.spec
 
@@ -189,21 +188,13 @@ def filter_spec(spec):
       env = spec['paths'][path][method]['x-prisma-cloud-target-env']
       tags = ep.get('tags', [])
       saas = env.get('saas')
-      if (('Supported API' in tags) and (saas is True)):
-        supported_paths[path][method] = copy.copy(ep)
-  spec['paths'] = copy.copy(supported_paths)
-  
-
-
-def filter_env_details(spec):
- 
-  for path in spec['paths']:
-    for method in spec['paths'][path]:
-      env = spec['paths'][path][method]['x-prisma-cloud-target-env']
-      if 'saas' in env:
+      if 'saas' in env: #remove the details that are needed to be published on pan.dev
         env.pop('saas')
       if 'self-hosted' in env:
         env.pop('self-hosted')
+      if (('Supported API' in tags) and (saas is True)):
+        supported_paths[path][method] = copy.copy(ep)
+  spec['paths'] = copy.copy(supported_paths)
 
 def remove_x_public(spec):
     for path in spec['paths']:
